e95a0325f20b5a639aecdb956621df1d23ca004e
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2016-11-17 20:42:24 +0100

    Version 0.1.0

--- pydefs/version.py
+++ pydefs/version.py
@@ -6,4 +6,4 @@ This module manages the version for pydefs.
 The version is defined by the Semantic Versioning convention (see http://semver.org).
 """
 
-__version__ = '0.0.0'
+__version__ = '0.1.0'
