dd5fcbcc27aa7b4c2e7c129c1240387a90f22879
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2016-11-17 20:00:14 +0100

    Add version module

---
+++ pydefs/version.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+
+"""
+This module manages the version for pydefs.
+
+The version is defined by the Semantic Versioning convention (see http://semver.org).
+"""
+
+__version__ = '0.0.0'
